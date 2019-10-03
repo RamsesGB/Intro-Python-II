9863a6a75f175008b204098abed62006a8b13109
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2019-10-02 22:50:28 -0400

    created classes for player room and object

---
+++ src/item.py
@@ -0,0 +1,10 @@
+class Item():
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
--- src/room.py
+++ src/room.py
@@ -1,2 +1,32 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+    def __init__(self, description, n_to, s_to, e_to, w_to, items=[]):
+        self.description = description
+        self.n_to = n_to
+        self.s_to = s_to
+        self.e_to = e_to
+        self.w_to = w_to
+        self.items = items
+
+    def add_items(self, item):
+        self.items.append(item)
+
+    def items_removed(self, item):
+        self.items.remove(item)
+
+    def items_available(self):
+        print(self.items)
+
+    def __str__(self):
+        return f"This is the {self.description} it is north to the {self.n_to} " \
+               f"south to the {self.s_to} east to the {self.e_to} and west to the {self.w_to} " \
+               f"in it you can find the {self.items}"
+
+    def __repr__(self):
+        pass
+
+
+living_room = Room("LivingRoom", "Kitchen", "Garage", "Backyard", "Foyer", "Remote")
+
+print(living_room)
